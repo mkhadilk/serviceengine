a4659b22d5aacaee7c549e6f4f58edf48f5bacde
Mandar Khadilkar <mkhadilk@gmail.com>
2024-05-07 17:46:35 -0700

    Adding Mongo Receiver Adaptor

---
+++ mongoadaptor/mongo.go
@@ -0,0 +1,154 @@
+package mongoadaptor
+
+import (
+	"context"
+	"errors"
+	"log"
+
+	"github.com/google/uuid"
+
+	"go.mongodb.org/mongo-driver/mongo/options"
+
+	se "github.com/mkhadilk/serviceengine"
+	"go.mongodb.org/mongo-driver/bson"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type MongoStatus string
+
+const (
+	MongoStatusAny       MongoStatus = ""
+	MongoStatusNew       MongoStatus = "new"
+	MongoStatusProcessed MongoStatus = "processed"
+)
+
+type MongoRequest struct {
+	ID         string      `json:"id" bson:"_id"` // Private field for ID
+	ExternalID string      `json:"externalid" bson:"externalid"`
+	Request    se.Request  `json:"request"`
+	Topic      string      `json:"topic" bson:"topic"` //optional
+	Status     MongoStatus `json:"mongostatus" bson:"mongostatus"`
+}
+
+func NewMongoRequest(req se.Request) *MongoRequest {
+	me := new(MongoRequest)
+	me.ID = req.GetID().String()
+	me.ExternalID = req.ExternalID
+	me.Request = req
+	me.Status = MongoStatusNew
+	return me
+}
+
+// This will insert only.
+func (m *MongoRequest) Save(cl *mongo.Collection) error {
+	if cl == nil {
+		return errors.New("Collection is not initilized...")
+	}
+	io, err := cl.InsertOne(nil, m)
+	if err != nil {
+		return err
+	}
+	if io.InsertedID != nil {
+		if err == nil {
+			return nil
+		} else {
+			return err
+		}
+	} else {
+		return errors.Join(errors.New("Can not save!"), err)
+	}
+}
+
+func (m *MongoRequest) Delete(cl *mongo.Collection) error {
+	if cl == nil {
+		return errors.New("Collection is not initilized...")
+	}
+	filter := bson.M{"_id": m.ID}
+	io, err := cl.DeleteOne(context.TODO(), filter)
+	if err != nil {
+		return err
+	}
+	if io.DeletedCount == 1 {
+		return nil
+	} else {
+		log.Printf("Deleted count %d is not equal to 1", io.DeletedCount)
+		return errors.Join(errors.New("Not Deleted!"), err)
+	}
+}
+
+// This will update or insert based on if the id exists or not
+func (m *MongoRequest) Update(cl *mongo.Collection) error {
+	u := new(options.UpdateOptions)
+	update := bson.M{"$set": *m}
+	ur, err := cl.UpdateByID(nil, m.ID, update, u.SetUpsert(true))
+	if err != nil || ur.UpsertedCount != 0 {
+		if err == nil {
+			return nil
+		} else {
+			return err
+		}
+	} else {
+		return errors.Join(errors.New("Can not insert or save!"), err)
+	}
+}
+
+// Pass any string as "" for id, externalid, topic to be ignored for filter and status as MongoStatusAny to ignore
+func Fetch(cl *mongo.Collection, id, externalid, topic string, status MongoStatus) ([]MongoRequest, error) {
+
+	var all []MongoRequest
+	all = make([]MongoRequest, 0)
+	filter := bson.M{}
+	if id != "" {
+		filter["_id"] = id
+	}
+	if externalid != "" {
+		filter["externalid"] = externalid
+	}
+	if topic != "" {
+		filter["topic"] = topic
+	}
+
+	if status != MongoStatusAny {
+		filter["mongostatus"] = status
+	}
+	var cur *mongo.Cursor
+	var err error
+	if len(filter) == 0 {
+		log.Println("Fetch all")
+		filter := bson.D{}
+		cur, err = cl.Find(context.TODO(), filter)
+	} else {
+		cur, err = cl.Find(context.TODO(), filter)
+	}
+	if err != nil {
+		return all, err
+	}
+
+	for cur.TryNext(context.Background()) {
+		var rec MongoRequest
+		err = cur.Decode(&rec)
+		if err == nil {
+			all = append(all, rec)
+		}
+	}
+
+	return all, err
+}
+
+func NewMongoConsumer(topic string, db *mongo.Database) *se.Consumer {
+	col := db.Collection(topic)
+	return se.NewConsumer(func(req se.Request) {
+		mr := NewMongoRequest(req)
+		mr.Topic = topic
+		mr.Status = MongoStatusNew
+		err := mr.Save(col)
+		res := se.NewResponse(uuid.MustParse(mr.ID), mr.ExternalID)
+		if err == nil {
+			res.Status = se.StatusError
+		} else {
+			res.Status = se.StatusReceived
+		}
+		req.Respond(*res)
+	})
+}
---
+++ mongoadaptor/mongo_test.go
@@ -0,0 +1,252 @@
+package mongoadaptor
+
+import (
+	"bytes"
+	"context"
+	"log"
+	"testing"
+	"time"
+
+	"github.com/google/uuid"
+
+	"go.mongodb.org/mongo-driver/mongo/options"
+
+	se "github.com/mkhadilk/serviceengine"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func getCollection(name string) *mongo.Collection {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017/?directConnection=true"))
+	if err != nil {
+		log.Printf("Error %+v", err)
+		return nil
+	}
+
+	// Connect to the MongoDB server
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Printf("Error %+v", err)
+		return nil
+	}
+
+	// Create a MongoDB collection
+
+	MONGODBNAME := "test"
+
+	db := client.Database(MONGODBNAME)
+
+	err = db.Client().Ping(nil, nil)
+
+	if err != nil {
+		log.Printf("error %+v", err)
+		return nil
+	}
+
+	return db.Collection(name)
+}
+
+func TestMongoMessageInsert(t *testing.T) {
+	col := getCollection("notifications")
+	if col == nil {
+		log.Println("Save failed; can not get collection")
+		t.FailNow()
+	}
+	re := se.NewRequest("extid")
+	body := struct{ Data string }{
+		Data: "testdata",
+	}
+	re.AsJSON(body)
+	m := NewMongoRequest(*re)
+	m.Status = MongoStatusNew
+
+	log.Printf("Saving %+v", *m)
+
+	err := m.Save(col)
+	if err != nil {
+		log.Printf("Save failed %+v", err)
+		col.Database().Client().Disconnect(nil)
+		t.FailNow()
+	}
+
+	col.Drop(nil)
+}
+
+func TestMongoMessageFetchSelective(t *testing.T) {
+
+	col := getCollection("notifications")
+	if col == nil {
+		log.Println("Save failed; can not get collection")
+		t.FailNow()
+	}
+	re := se.NewRequest("extid")
+	body := struct{ Data string }{
+		Data: "testdata",
+	}
+	re.AsJSON(body)
+	m := NewMongoRequest(*re)
+	m.Status = MongoStatusNew
+
+	err := m.Update(col)
+
+	if err != nil {
+		log.Printf("Insert/Update failed %+v", err)
+		col.Database().Client().Disconnect(nil)
+		t.FailNow()
+	}
+
+	log.Printf("Fetching %+v", *m)
+
+	reqs, err := Fetch(col, m.ID, "", "", MongoStatusAny)
+	if err != nil {
+		log.Printf("Fetch failed %+v", err)
+		col.Database().Client().Disconnect(nil)
+		t.FailNow()
+	}
+
+	log.Printf("Records found %+v", reqs)
+
+	if len(reqs) != 1 || reqs[0].ID != m.ID || reqs[0].ExternalID != m.ExternalID || bytes.Compare(reqs[0].Request.Message, m.Request.Message) != 0 {
+		log.Printf("Fetch failed %+v is not same as %+v", reqs, *m)
+		t.FailNow()
+	}
+
+	col.Drop(nil)
+}
+func TestMongoMessageDeleteSelective(t *testing.T) {
+
+	col := getCollection("notifications")
+	if col == nil {
+		log.Println("Save failed; can not get collection")
+		t.FailNow()
+	}
+	re := se.NewRequest("extid")
+	body := struct{ Data string }{
+		Data: "testdata",
+	}
+	re.AsJSON(body)
+	m := NewMongoRequest(*re)
+	m.Status = MongoStatusNew
+
+	err := m.Update(col)
+
+	if err != nil {
+		log.Printf("Insert/Update failed %+v", err)
+		col.Database().Client().Disconnect(nil)
+		t.FailNow()
+	}
+
+	log.Printf("Deleting %+v", *m)
+
+	err = m.Delete(col)
+
+	log.Printf("Delete error %+v", err)
+
+	reqs, err := Fetch(col, m.ID, "", "", MongoStatusAny)
+	if err != nil {
+		log.Printf("Fetch failed %+v", err)
+		col.Database().Client().Disconnect(nil)
+		t.FailNow()
+	}
+
+	log.Printf("Records found %+v", reqs)
+
+	if len(reqs) != 0 {
+		log.Printf("Delete failed; expected 0 but found %d", len(reqs))
+		t.FailNow()
+	}
+
+	col.Drop(nil)
+}
+func TestMongoMessageFetchAll(t *testing.T) {
+	col := getCollection("notifications")
+	if col == nil {
+		log.Println("Save failed; can not get collection")
+		t.FailNow()
+	}
+	re := se.NewRequest("extid")
+	body := struct{ Data string }{
+		Data: "testdata",
+	}
+	re.AsJSON(body)
+	m := NewMongoRequest(*re)
+	m.Status = MongoStatusNew
+
+	err := m.Update(col)
+
+	if err != nil {
+		log.Printf("Insert/Update failed %+v", err)
+		col.Database().Client().Disconnect(nil)
+		t.FailNow()
+	}
+
+	log.Printf("Fetching %+v", *m)
+
+	reqs, err := Fetch(col, "", "", "", MongoStatusAny)
+	if err != nil {
+		log.Printf("Fetch failed %+v", err)
+		col.Database().Client().Disconnect(nil)
+		t.FailNow()
+	}
+
+	log.Printf("Records found %+v", reqs)
+
+	if len(reqs) < 1 {
+		log.Printf("Fetch failed; records found %d; execting at least 1", len(reqs))
+	}
+
+	col.Drop(nil)
+}
+
+func TestMongoPublicSubscribe(t *testing.T) {
+	topic := "serviceproviders"
+	se.ENGINE.AddConsumer(topic, *NewMongoConsumer(topic, getCollection(topic).Database()))
+
+	re := se.NewRequest(uuid.New().String())
+	body := struct{ Data string }{
+		Data: "SP message",
+	}
+	re.AsJSON(body)
+
+	log.Printf("Message with ID %s Message\n%+v", re.GetID().String(), *re)
+
+	se.ENGINE.Execute(topic, *re)
+	res := re.Receive()
+
+	log.Printf("Received %+v", res)
+
+	lst, err := Fetch(getCollection(topic), re.GetID().String(), "", "", MongoStatusAny)
+
+	if err != nil {
+		log.Printf("Error %+v", err)
+		t.Fail()
+	}
+
+	log.Printf("returned requests %+v", lst)
+
+	if len(lst) != 1 {
+		log.Printf("Error %+v", err)
+		t.FailNow()
+	}
+
+	if lst[0].ID != re.GetID().String() {
+		log.Printf("Error wrong message ID %s expected; actual %s", re.GetID().String(), lst[0].ID)
+		t.FailNow()
+	}
+
+	var body2 struct{ Data string }
+
+	lst[0].Request.FromJSONInto(&body2)
+
+	log.Printf("Notification \nSent %+v\nReveiced %+v", body, body2)
+
+	if body.Data != body2.Data {
+		log.Printf("Error wrong message Data for ID %s; expected %s; actual %s", re.GetID().String(), body.Data, body2.Data)
+		t.FailNow()
+	}
+
+	getCollection(topic).Drop(nil)
+
+}
